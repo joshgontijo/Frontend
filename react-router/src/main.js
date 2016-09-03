afde1b94e1c04445b18405cb79aaf61aee836e61
Josue Gontijo <josue.eduardo206@gmail.com>
2016-09-03 22:10:23 +0100

    Added animatino to reat-router

--- react-router/src/main.js
+++ react-router/src/main.js
@@ -2,69 +2,62 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, IndexRoute, Link, IndexLink, browserHistory } from 'react-router'
 
-const ACTIVE = {color: 'red'};
+import EasyTransition from 'react-easy-transition'
 
-const App = ({ children }) => (
+const App = ({ children, location }) => (
     <div>
-        <h1>APP!</h1>
         <ul>
-            <li><Link to="/" activeStyle={ACTIVE}>/</Link></li>
-            <li><IndexLink to="/" activeStyle={ACTIVE}>/ IndexLink</IndexLink></li>
-
-            <li><Link to="/users" activeStyle={ACTIVE}>/users</Link></li>
-            <li><IndexLink to="/users" activeStyle={ACTIVE}>/users IndexLink</IndexLink></li>
-
-            <li><Link to="/users/ryan" activeStyle={ACTIVE}>/users/ryan</Link></li>
-            <li><Link to={{ pathname: '/users/ryan', query: { foo: 'bar' } }}
-                      activeStyle={ACTIVE}>/users/ryan?foo=bar</Link></li>
-
-            <li><Link to="/about" activeStyle={ACTIVE}>/about</Link></li>
+            <li><Link to="/page1">Page 1</Link></li>
+            <li><Link to="/page2">Page 2</Link></li>
         </ul>
 
-        {children}
-    </div>
-);
+        <EasyTransition
+            path={location.pathname}
+            initialStyle={{opacity: 0}}
+            transition="opacity 0.5s ease-in"
+            finalStyle={{opacity: 1}}>
 
-const Index = () => (
-    <div>
-        <h2>Index!</h2>
-    </div>
-);
 
-const Users = ({ children }) => (
-    <div>
-        <h2>Users</h2>
-        {children}
-    </div>
-);
+            {children}
+        </EasyTransition>
 
-const UsersIndex = () => (
-    <div>
-        <h3>UsersIndex</h3>
     </div>
-);
+)
 
-const User = ({ params: { id } }) => (
-    <div>
-        <h3>User {id}</h3>
+const Index = () => ( 
+    <div className="Image">
+        <h1>Index</h1>
+        <p>Animations with React Router with <a href="https://github.com/misterfresh/react-easy-transition">react-easy-transition</a>.</p>
     </div>
-);
+)
 
-const About = () => (
-    <div>
-        <h2>About</h2>
+const Page1 = () => (
+    <div className="Image">
+        <h1>Page 1</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.</p>
     </div>
-);
+)
+
+const Page2 = () => (
+    <div className="Image">
+        <h1>Page 2</h1>
+        <p>Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+            minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+            occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+)
 
 render((
     <Router history={browserHistory}>
         <Route path="/" component={App}>
             <IndexRoute component={Index}/>
-            <Route path="/about" component={About}/>
-            <Route path="users" component={Users}>
-                <IndexRoute component={UsersIndex}/>
-                <Route path=":id" component={User}/>
-            </Route>
+            <Route path="page1" component={Page1}/>
+            <Route path="page2" component={Page2}/>
         </Route>
     </Router>
-), document.getElementById('root'));
+), document.getElementById('root'))
